8696b8f346e2d75abe7537cd69e505298df4791b
ShivanshRakesh <49039969+ShivanshRakesh@users.noreply.github.com>
2020-04-27 22:47:34 +0530

    Updated License.

--- src/components/settings.js
+++ src/components/settings.js
@@ -1,7 +1,7 @@
 /*
 BSD 3-Clause License
 
-Copyright 2020, Shivansh
+Copyright (c) 2020, Shivansh All rights reserved.
 
 Redistribution and use in source and binary forms, with or without 
 modification, are permitted provided that the following conditions are met:
